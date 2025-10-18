e34cd52556b653f798151c3c173a4fb916649c8e
Mounish R <mounishr6680@gmail.com>
2025-10-18 12:24:26 +0530

    Add files via upload

---
+++ demo/demo/src/main/java/Hospital_Mangement/com/example/demo/HospitalMangementApplication.java
@@ -0,0 +1,13 @@
+package Hospital_Mangement.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalMangementApplication.class, args);
+	}
+
+}
