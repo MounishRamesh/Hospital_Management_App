7f9fb83a6eb4e7e8e2c2dcef4c9ac3be439511ab
Mounish R <mounishr6680@gmail.com>
2025-10-22 00:16:35 +0530

    Delete demo/demo directory

--- demo/demo/src/main/java/Hospital_Mangement/com/example/demo/repository/BillingRepository.java
+++
@@ -1,9 +0,0 @@
-package Hospital_Mangement.com.example.demo.repository;
-
-import Hospital_Mangement.com.example.demo.model.Billing;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillingRepository extends JpaRepository<Billing, Long> {
-}
